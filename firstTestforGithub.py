d701d7f6725886ecc0f445e3976eda16c8a29eef
hsiaofeiker <adasteve.hlf@gmail.com>
2020-02-18 22:23:21 -0800

    第一次測試 用 PyCharm中的 Git上傳功能 , 傳到 Github

---
+++ firstTestforGithub.py
@@ -0,0 +1,15 @@
+
+
+
+my_list=["Jake","Tome","xi Li"]
+
+
+
+#for name in my_list:
+#   print(name)
+
+# for number in range(11);
+#   print(number)
+
+for letter in 'inty':
+        print("each letter is: "+letter)
